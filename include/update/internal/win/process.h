dea3ee32656c36ea37dde2efeadc382ac62ea578
Jonas van den Berg <jonas@vandenbe.com>
2024-08-15 19:03:54 +0200

    Fix compiler errors

--- include/update/internal/win/process.h
+++ include/update/internal/win/process.h
@@ -4,8 +4,12 @@
 #include <optional>
 #include <sstream>
 
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <Windows.h>
 #include <winrt/base.h>
 // #include <handleapi.h>
